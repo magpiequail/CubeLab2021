52f46a4ab528a2b28c98a9ab5deb8d13b3b49976
magpiequail <48472215+magpiequail@users.noreply.github.com>
2021-08-17 21:34:49 +0900

    test

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -19,11 +19,7 @@ public class Character : MonoBehaviour
     public bool isUnitMoveAllowed = true;
     //bool isInputAllowed = true;
     public bool isDeathByLaser = false;
-    /*public bool isHavingRoundKey = false;
-    public bool isHavingTriangleKey = false;
-    public bool isHavingDiamondKey = false;
-    public bool isHavingSquareKey = false;*/
-    // public GameObject floor;
+
     protected float keyPressedTime = 0f;
 
     public int rows = 5;
@@ -110,117 +106,7 @@ public class Character : MonoBehaviour
         //Debug.DrawLine(currPos, transform.forward,Color.red);
 
 
-        //transform.position = Vector3.MoveTowards(transform.position, fl.gridArray[fl.charPosX, fl.charPosY].transform.position, speed * Time.deltaTime);
 
-        //Vector3 dist = transform.position - fl.gridArray[fl.charPosX, fl.charPosY].transform.position;
-        //if (dist.sqrMagnitude > 0f) //move
-        //{
-        //    characterAnim.SetInteger("Idle", 0);
-        //    //isUnitMoveAllowed = false;
-
-        //}
-        //else //idle
-        //{
-        //    characterAnim.SetInteger("Idle", 1);
-        //    //isUnitMoveAllowed = true;
-        //}
-
-
-        ////for keyboard inputs
-
-        //if (isInputAllowed)
-        //{
-        //    if (Input.GetKey(KeyCode.A))
-        //    {
-        //        keyPressedTime += Time.deltaTime;
-        //        if (keyPressedTime > inputTriggerTime)
-        //        {
-        //            keyPressedTime = inputTriggerTime - inputWaitTime;
-        //            fl.SWMovement();
-        //            //characterAnim.SetInteger("Idle", 0);
-        //        }
-        //    }
-        //    if (Input.GetKey(KeyCode.S))
-        //    {
-        //        keyPressedTime += Time.deltaTime;
-        //        if (keyPressedTime > inputTriggerTime)
-        //        {
-        //            keyPressedTime = inputTriggerTime - inputWaitTime;
-        //            fl.SEMovement();
-        //            //characterAnim.SetInteger("Idle", 0);
-        //        }
-        //    }
-        //    if (Input.GetKey(KeyCode.Q))
-        //    {
-        //        keyPressedTime += Time.deltaTime;
-        //        if (keyPressedTime > inputTriggerTime)
-        //        {
-        //            keyPressedTime = inputTriggerTime - inputWaitTime;
-        //            fl.NWMovement();
-        //            //characterAnim.SetInteger("Idle", 0);
-        //        }
-        //    }
-        //    if (Input.GetKey(KeyCode.W))
-        //    {
-        //        keyPressedTime += Time.deltaTime;
-        //        if (keyPressedTime > inputTriggerTime)
-        //        {
-        //            keyPressedTime = inputTriggerTime - inputWaitTime;
-        //            fl.NEMovement();
-        //            //characterAnim.SetInteger("Idle", 0);
-        //        }
-        //    }
-        //    if (isUnitMoveAllowed)
-        //    {
-
-
-        //        if (Input.GetKeyDown(KeyCode.A))
-        //        {
-        //            fl.SWMovement();
-        //            //characterAnim.Play("Idle_SW");
-        //            //characterAnim.SetTrigger("Jump");
-        //            characterAnim.SetInteger("Direction", 3);
-        //            characterAnim.Play("Walk");
-        //            keyPressedTime = 0f;
-        //        }
-
-        //        if (Input.GetKeyDown(KeyCode.S))
-        //        {
-        //            fl.SEMovement();
-        //            //characterAnim.Play("Idle_SE");
-        //            //characterAnim.SetTrigger("Jump");
-        //            characterAnim.SetInteger("Direction", 4);
-        //            characterAnim.Play("Walk_SE");
-        //            keyPressedTime = 0f;
-        //        }
-
-        //        if (Input.GetKeyDown(KeyCode.Q))
-        //        {
-        //            fl.NWMovement();
-        //            //characterAnim.Play("Idle_NW");
-        //            //characterAnim.SetTrigger("Jump");
-        //            characterAnim.SetInteger("Direction", 1);
-        //            characterAnim.Play("Walk_NW");
-        //            keyPressedTime = 0f;
-        //        }
-        //        if (Input.GetKeyDown(KeyCode.W))
-        //        {
-        //            fl.NEMovement();
-        //            //characterAnim.Play("Idle_NE");
-        //            //characterAnim.SetTrigger("Jump");
-        //            characterAnim.SetInteger("Direction", 2);
-        //            characterAnim.Play("Walk_NE");
-        //            keyPressedTime = 0f;
-        //        }
-        //        if (Input.GetKeyUp(KeyCode.A)
-        //            || Input.GetKeyUp(KeyCode.S)
-        //            || Input.GetKeyUp(KeyCode.Q)
-        //            || Input.GetKeyUp(KeyCode.W))
-        //        {
-        //            keyPressedTime = 0f;
-        //        }
-        //    }
-        //}
 
 
         if (Physics2D.OverlapCircle(nextPos, 0.1f, accessible))
@@ -240,23 +126,7 @@ public class Character : MonoBehaviour
 
     }
 
-    /*public virtual void Initialize()
-    {
-        grid = FindObjectOfType<Grid>();
-        gridX = grid.cellSize.x / 2;
-        gridY = grid.cellSize.y / 2;
 
-        currPos = transform.position;
-        nextPos = transform.position;
-        cm = FindObjectOfType<CharactersMovement>();
-
-        characterAnim = GetComponentInChildren<Animator>();
-        characterAnim.SetInteger("Idle", 1);
-        characterAnim.SetInteger("StageClear", 0);
-        characterAnim.SetInteger("Direction", 3);
-
-        SetCurrentBlock();
-    }*/
 
     public virtual void UpdateFunc()
     {
@@ -296,104 +166,26 @@ public class Character : MonoBehaviour
 
     public virtual bool SWMovement()
     {
-        /*if (!isUnitMoveAllowed)
-        {
-            return false;
-        }
-        ResetBlockColor();
-        nextPos = new Vector2(currPos.x - gridX, currPos.y - gridY);
-        characterAnim.SetInteger("Direction", 3);
-        if (!Physics2D.OverlapCircle(nextPos, 0.1f, accessible))
-        {
-            nextPos = currPos;
-            return false;
-        }
 
-        SetCurrentBlock();
-        characterAnim.SetInteger("Idle", 0);
-        characterAnim.Play("Walk_SW");*/
 
         return true;
     }
     public virtual bool SEMovement()
     {
-        /*if (!isUnitMoveAllowed)
-        {
-            return false;
-        }
-        ResetBlockColor();
-        nextPos = new Vector2(currPos.x + gridX, currPos.y - gridY);
-        characterAnim.SetInteger("Direction", 4);
-
-        if (!Physics2D.OverlapCircle(nextPos, 0.1f, accessible))
-        {
-            nextPos = currPos;
-            return false;
-        }
-
-        SetCurrentBlock();
-        characterAnim.SetInteger("Idle", 0);
-        characterAnim.Play("Walk_SE");*/
 
         return true;
     }
     public virtual bool NWMovement()
     {
-        /*if (!isUnitMoveAllowed)
-        {
-            return false;
-        }
-        ResetBlockColor();
-        nextPos = new Vector2(currPos.x - gridX, currPos.y + gridY);
-        characterAnim.SetInteger("Direction", 1);
-        if (!Physics2D.OverlapCircle(nextPos, 0.1f, accessible) )
-        {
-            nextPos = currPos;
-            return false;
-        }
 
-        SetCurrentBlock();
-        characterAnim.Play("Walk_NW");
-        characterAnim.SetInteger("Idle", 0);*/
         return true;
     }
 
     public virtual bool NEMovement()
     {
-        /*if (!isUnitMoveAllowed)
-        {
-            return false;
-        }
-        ResetBlockColor();
-        nextPos = new Vector2(currPos.x + gridX, currPos.y + gridY);
-        characterAnim.SetInteger("Direction", 2);
-        if (!Physics2D.OverlapCircle(nextPos, 0.1f, accessible))
-        {
-            nextPos = currPos;
-            return false;
-        }
 
-        SetCurrentBlock();
-        characterAnim.Play("Walk_NE");
-        characterAnim.SetInteger("Idle", 0);*/
         return true;
     }
-
-    //this function is not being used
-    /*void GetTile()
-    {
-        tmc.tilemap.RefreshAllTiles();
-        
-        Vector3 worldPoint = CameraManager.currentCam.ScreenToWorldPoint(nextPos);
-        RaycastHit2D hit = Physics2D.Raycast(worldPoint, transform.forward);
-        Debug.DrawRay(worldPoint, transform.forward * 10, Color.red, 0.3f);
-        if (hit)
-        {
-        tmc.x = tmc.tilemap.WorldToCell(nextPos).x;
-        tmc.y = tmc.tilemap.WorldToCell(nextPos).y;
-
-        }
-    }*/
 
 
     //these functions are used for mouse click movement
